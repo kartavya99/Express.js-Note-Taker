f64a1d10a3ecf290bb74ff13cf847cecb6262873
kartavya <kartavyas@gmail.com>
2022-05-06 16:00:43 +1000

    modular rotuter imported for notes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
-//The following HTML routes should be created:
 
-// `GET /notes` should return the `notes.html` file.
+const express = require("express");
 
-// `GET *` should return the `index.html` file.
+// Import our modular router for notes
+const nRouter = require("./notesRouter");
 
-// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+const app = express();
+
+app.use("/notes", nRouter);
+
+module.exports = app;
