456dd0143b61790485c2555dc03b9886e88b597d
kartavya <kartavyas@gmail.com>
2022-05-06 14:59:12 +1000

    routes file updated with comments

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+//The following HTML routes should be created:
+
+// `GET /notes` should return the `notes.html` file.
+
+// `GET *` should return the `index.html` file.
+
+// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
