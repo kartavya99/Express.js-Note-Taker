fd2c540b4d1a06333a53a47c7c3b7f8dfb1e5fff
kartavya <kartavyas@gmail.com>
2022-05-06 15:16:35 +1000

    server.js updated with Middleware

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 
 const express = require("express");
 const path = require("path");
+const api = require("./routes/index.js");
+
 
 
 // set up express function with app variable 
 // set up Port variable on 3001
 const app = express();
-const PORT = 3001;
+
+// changin port variable into enviroment variable for Heroku to listen
+const PORT = process.env.PORT || 3001;
+
+// Setting up Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 
 
